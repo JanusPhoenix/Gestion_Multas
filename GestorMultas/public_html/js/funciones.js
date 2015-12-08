f8f6d8ff685c7ca8f9a8c33ef6438dfd5cf1528e
AntonioReina <antonioalvarezreina@gmail.com>
2015-12-08 19:49:40 +0100

    Creacion de objetos Creacion de formularios Creacion de altaCliente

--- GestorMultas/public_html/js/funciones.js
+++ GestorMultas/public_html/js/funciones.js
@@ -6,8 +6,8 @@ function inicio(){
     document.getElementById("usu").addEventListener('click',mostrarFomularioCliente,false);
     document.getElementById("altaUsuario").addEventListener('click',mostrarAltaUsuario,false);
     //document.altaUsu.addEventListener("submit",validar,false);
-    document.altaUsu.addEventListener("submit",clienteN,false);
-    document.getElementById("boton").addEventListener('click',ocultarMensaje,false)
+    document.getElementById("enviar").addEventListener("click",clienteN,false);
+    document.getElementById("boton").addEventListener('click',ocultarMensaje,false);
 }
 //-----------------Funciones para mostrar formularios y mensajes--------------//
 //Funcion para mostrar los mensajes
@@ -46,77 +46,83 @@ function clienteN(){
     sNombre = document.altaUsu.nombre.value;
     sApellido = document.altaUsu.ape.value;   
     sNif = document.altaUsu.nif.value;
-    
-    if(validar(sNombre,sApellido,sNif) == true){
+    if(validarUsuario() == true){
         oCliente = new Cliente(sNif,sNombre,sApellido);
         mensaje(oGestorMultas.altaCliente(oCliente));
-    }    
+    }
 }
 //--------------------------Funciones de validacion---------------------------//
-function validar(oEvento,sNombre,sApellido,sNif){
-    var oE = oEvento || window.event;
-	var bValido = true;
-	var sErrores = "";
-        	       
+function validarUsuario(sNombre,sApellido,sNif){
+    //var oE = oEvento || window.event;
+    var bValido = true;
+    var sErrores = "";
+    var oCliente;	       
         //Validacion del nombre
-//	var sNombre = document.altaUsu.nombre.value.trim();
-//	document.altaUsu.nombre.value = document.altaUsu.nombre.value.trim();
+//    var 
+    sNombre = document.altaUsu.nombre.value.trim();
+//    document.altaUsu.nombre.value = document.altaUsu.nombre.value.trim();
 
-	var oExpReg = /[a-zA-Z\s]{3,40}/;
-    
-	if (oExpReg.test(sNombre) == false){
-            if(bValido == true){
-                bValido = false;		
-                document.altaUsu.nombre.focus();		
-            }
-            sErrores += "\nNombre incorrecto";
-            document.altaUsu.nombre.className = "form-control error";//Marcaciones de error, si entra lo marcara, si pasa por el else lo desmarca.
-	}else{
-            document.altaUsu.nombre.className = "form-control";	
-	}
+    var oExpReg = /[a-zA-Z\s]{3,40}/;
+
+    if (oExpReg.test(sNombre) == false){
+        if(bValido == true){
+            bValido = false;		
+            document.altaUsu.nombre.focus();		
+        }
+        sErrores += "\nNombre incorrecto";
+        document.altaUsu.nombre.className = "form-control error";//Marcaciones de error, si entra lo marcara, si pasa por el else lo desmarca.
+    }else{
+        document.altaUsu.nombre.className = "form-control";	
+    }
         
 	//Validacion del apellido
-//	var sApellido = document.altaUsu.ape.value.trim();
-//	document.altaUsu.ape.value = document.altaUsu.ape.value.trim();
+//    var 
+    sApellido = document.altaUsu.ape.value.trim();
+//    document.altaUsu.ape.value = document.altaUsu.ape.value.trim();
                
-	if (oExpReg.test(sApellido) == false){
-	
-            if(bValido == true){
-                bValido = false;		
-                document.altaUsu.ape.focus();		
-            }
-            sErrores += "\nApellidos incorrecto";
-            document.altaUsu.ape.className = "form-control error";
-	
-	}
-	else {
-            document.contacto.ape.className = "form-control";	
-	}
+    if (oExpReg.test(sApellido) == false){
+
+        if(bValido == true){
+            bValido = false;		
+            document.altaUsu.ape.focus();		
+        }
+        sErrores += "\nApellidos incorrecto";
+        document.altaUsu.ape.className = "form-control error";
+
+    }
+    else {
+        document.altaUsu.ape.className = "form-control";	
+    }
         
         //validacion del campo nif
-//        var sNif = document.altaUsu.nif.value.trim();
-//        document.altaUsu.nif.value = document.altaUsu.nif.value.trim();
+//    var 
+      sNif = document.altaUsu.nif.value.trim();
+//    document.altaUsu.nif.value = document.altaUsu.nif.value.trim();
         
-        var oExpReg2 = /[0-9]{7}[A-Z]/;
-        
-        if (oExpReg2.test(sNif) == false){
-            if(bValido == true){
-                bValido = false;		
-                document.altaUsu.nif.focus();		
-            }
-            sErrores += "\nNif incorrecto";
-            document.altaUsu.nif.className = "form-control error";	
-	}
-	else {
-            document.contacto.nif.className = "form-control";	
-	}
+    var oExpRegNif = /[0-9]{7}[A-Z]/;
+
+    if (oExpRegNif.test(sNif) == false){
+        if(bValido == true){
+            bValido = false;		
+            document.altaUsu.nif.focus();		
+        }
+        sErrores += "\nNif incorrecto";
+        document.altaUsu.nif.className = "form-control error";	
+    }
+    else {
+        document.altaUsu.nif.className = "form-control";	
+    }
                 
-	//Condicion para enviar formulario solo si todos los datos son correctos
-	if (bValido == false){
-            //Cancelar envio del formulario
-            oE.preventDefault();
-            //Mostrar errores
-            alert(sErrores);
-	}
-	return bValido;
+    //Condicion para enviar formulario solo si todos los datos son correctos
+    if (bValido == false){
+        //Cancelar envio del formulario
+//        oE.preventDefault();
+        //Mostrar errores
+        mensaje(sErrores);
+    }
+//    else{
+//        oCliente = new Cliente(sNif,sNombre,sApellido);
+//        mensaje(oGestorMultas.altaCliente(oCliente));
+//    }
+    return bValido;
 }
